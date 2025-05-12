2494d2d528fa9160994c273a926d25b266abf629
GabrielPG17 <gabs.bet17@gmail.com>
2025-05-12 08:37:57 -0300

    teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,11 @@
-from main import analisar_lista
+def analisar_lista(lista_numeros):
+  
+    media = sum(lista_numeros) / len(lista_numeros)
+    maior = max(lista_numeros)
+    menor = min(lista_numeros)
+    pares = sum(1 for num in lista_numeros if num % 2 == 0)
+    
+    return {'media': media, 'maior': maior, 'menor': menor, 'pares': pares}
 
 numeros = [10, 23, 5, 8, 12, 33, 42, 7, 19, 28, 3, 16, 9, 50, 21]
 
