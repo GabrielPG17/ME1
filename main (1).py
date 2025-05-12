370f326a81998d84766584dfb301a78d43b7c228
GabrielPG17 <gabs.bet17@gmail.com>
2025-05-12 08:43:58 -0300

    main.py

--- main (1).py
+++ main (1).py
@@ -1,15 +1,17 @@
-def analisar_lista(numeros):
-    if not numeros:
-        return "Lista vazia."
+def analisar_lista(lista_numeros):
+  
+    media = sum(lista_numeros) / len(lista_numeros)
+    maior = max(lista_numeros)
+    menor = min(lista_numeros)
+    pares = sum(1 for num in lista_numeros if num % 2 == 0)
+    
+    return {'media': media, 'maior': maior, 'menor': menor, 'pares': pares}
 
-    media = sum(numeros) / len(numeros)
-    maior = max(numeros)
-    menor = min(numeros)
-    pares = len([n for n in numeros if n % 2 == 0])
+numeros = [analisar_lista]
 
-    return {
-        "media": round(media, 2),
-        "maior": maior,
-        "menor": menor,
-        "pares": pares
-    }
+resultado = analisar_lista(numeros)
+
+print(f"Média: {resultado['media']}")
+print(f"Maior número: {resultado['maior']}")
+print(f"Menor número: {resultado['menor']}")
+print(f"Quantidade de números pares: {resultado['pares']}")
